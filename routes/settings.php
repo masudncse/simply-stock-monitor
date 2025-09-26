ed87d45b2a8446d97e3db0d2192299a8944f5c54
Md. Masudul Kabir <masud.ncse@gmail.com>
2025-09-26 21:43:20 +0600

    Update

--- routes/settings.php
+++ routes/settings.php
@@ -20,7 +20,7 @@ Route::middleware('auth')->group(function () {
         ->name('password.update');
 
     Route::get('settings/appearance', function () {
-        return Inertia::render('settings/appearance');
+        return Inertia::render('Settings/Appearance');
     })->name('appearance.edit');
 
     Route::get('settings/two-factor', [TwoFactorAuthenticationController::class, 'show'])
